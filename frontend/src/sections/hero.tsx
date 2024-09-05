91b2682019033fd567d40153c02a5d3a6a3c5611
Pavel Sinelnikov <pavel@sinelnikov.xyz>
2024-09-04 18:21:51 -0600

    Refactored docs structure

--- frontend/src/sections/hero.tsx
+++ frontend/src/sections/hero.tsx
@@ -1,17 +1,23 @@
-
-'use client';
-import Block from "@/components/ui/block";
-import Link from 'next/link';
-import { motion } from "framer-motion";
-import { FiArrowRight, FiMail, FiMapPin } from "react-icons/fi";
-import { FaPlus } from "react-icons/fa";
-import { SiDocusaurus, SiDocusign, SiGithub, SiTiktok, SiTwitter, SiYoutube   } from "react-icons/si";
-import { SlDocs } from "react-icons/sl";
+'use client'
+import Block from '@/components/ui/block'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+import { FiArrowRight, FiMail, FiMapPin } from 'react-icons/fi'
+import { FaPlus } from 'react-icons/fa'
+import {
+  SiDocusaurus,
+  SiDocusign,
+  SiGithub,
+  SiTiktok,
+  SiTwitter,
+  SiYoutube,
+} from 'react-icons/si'
+import { SlDocs } from 'react-icons/sl'
 import { Badge } from '@/components/ui/badge'
-import { FaDiscord } from "react-icons/fa";
-import { PiIslandFill } from "react-icons/pi";
+import { FaDiscord } from 'react-icons/fa'
+import { PiIslandFill } from 'react-icons/pi'
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -21,16 +27,13 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { Progress } from '@/components/ui/progress'
-import { useState, useEffect } from 'react';
-import Content from "@/components/content";
+import { useState, useEffect } from 'react'
+import Content from '@/components/content'
 import Image from 'next/image'
-
-
-
 
 export default function Hero() {
   return (
-    <section className="mb-14 px-5 dark:bg-darkBg inset-0 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
+    <section className="inset-0 mb-14 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px] px-5 dark:bg-darkBg">
       <div className="mx-auto w-full max-w-full px-12 py-12  lg:py-[150px]">
         <motion.div
           initial="initial"
@@ -41,106 +44,114 @@ export default function Hero() {
           className="mx-auto grid grid-flow-dense grid-cols-12 gap-4"
         >
           <HeaderBlock />
-          <AboutBlock2 />
+          {/* <AboutBlock2 /> */}
           <SocialsBlock />
-          <ProjectBlock />
-          <AboutBlock />
-          <LocationBlock />
-          <EmailListBlock />
+          {/* <ProjectBlock /> */}
+          {/* <AboutBlock /> */}
+          {/* <LocationBlock />
+          <EmailListBlock /> */}
         </motion.div>
       </div>
     </section>
   )
 }
 
-
-{/* <img
+{
+  /* <img
       src="https://api.dicebear.com/8.x/lorelei-neutral/svg?seed=John"
       alt="avatar"
       className="mb-4 size-14 rounded-full"
-    /> */}
+    /> */
+}
 
 const HeaderBlock = () => {
-  const [progress, setProgress] = useState(13);
+  const [progress, setProgress] = useState(13)
 
   useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500);
-    return () => clearTimeout(timer);
-  }, []);
+    const timer = setTimeout(() => setProgress(66), 500)
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
     <Block className="col-span-12 row-span-2 md:col-span-6">
       <Card>
         <CardHeader>
-          <CardTitle className="text-darkBg">
-            Welcome to ZKM
-          </CardTitle>
-          <CardDescription>Get started with our general-purpose zkVM</CardDescription>
+          <CardTitle className="text-darkBg">Welcome to ZKM</CardTitle>
+          <CardDescription>
+            Get started with our general-purpose zkVM
+          </CardDescription>
         </CardHeader>
         <CardContent>
           <p>
-            By utilizing the power of zkMIPS
-            We have created the Entangled Rollup Network where blockchains can share native assets with no added security assumptions.
+            By utilizing the power of zkMIPS We have created the Entangled
+            Rollup Network where blockchains can share native assets with no
+            added security assumptions.
           </p>
         </CardContent>
-        <CardFooter className="flex space-x-3">
-          <Badge variant={"neutral"} >zkmup</Badge>
-          <Badge variant={"neutral"}>cargo zkm --version</Badge>
-          <Badge variant={"neutral"}>cargo zkm new my_project</Badge>
-        </CardFooter>
+        {/* <CardFooter className="flex space-x-3">
+          <Badge variant={'neutral'}>zkmup</Badge>
+          <Badge variant={'neutral'}>cargo zkm --version</Badge>
+          <Badge variant={'neutral'}>cargo zkm new my_project</Badge>
+        </CardFooter> */}
       </Card>
     </Block>
-  );
-};
+  )
+}
 
 const SocialsBlock = () => (
   <>
     <Block
       whileHover={{
-        rotate: "2.5deg",
+        rotate: '2.5deg',
         scale: 1.1,
       }}
       className="col-span-6 bg-darkBg md:col-span-3"
     >
       <a
-        href="#"
+        href="https://discord.gg/zkm"
         className="grid h-full place-content-center text-3xl text-white"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        <div className="flex space-x-2 items-center">
-          <FaDiscord  />
-          <p>Dev Hub</p>
+        <div className="flex items-center space-x-2">
+          <FaDiscord />
+          <p>Discord</p>
         </div>
       </a>
     </Block>
     <Block
       whileHover={{
-        rotate: "-2.5deg",
+        rotate: '-2.5deg',
         scale: 1.1,
       }}
       className="col-span-6 bg-darkBg md:col-span-3"
     >
       <a
-        href="#"
+        href="https://github.com/zkMIPS/zkm"
         className="grid h-full place-content-center text-3xl text-white"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        <div className="flex space-x-2 items-center">
-          <SiGithub  />
+        <div className="flex items-center space-x-2">
+          <SiGithub />
           <p>Github</p>
         </div>
       </a>
     </Block>
     <Block
       whileHover={{
-        rotate: "-2.5deg",
+        rotate: '-2.5deg',
         scale: 1.1,
       }}
       className="col-span-6 bg-darkBg md:col-span-3"
     >
       <a
-          href="https://playground.zkm.io/"
-          className="grid h-full place-content-center text-3xl text-white"
-          target="_blank" rel="noopener noreferrer">
-        <div className="flex space-x-2 items-center">
+        href="https://playground.zkm.io/"
+        className="grid h-full place-content-center text-3xl text-white"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="flex items-center space-x-2">
           <PiIslandFill />
           <p>Playground</p>
         </div>
@@ -148,14 +159,14 @@ const SocialsBlock = () => (
     </Block>
     <Block
       whileHover={{
-        rotate: "2.5deg",
+        rotate: '2.5deg',
         scale: 1.1,
       }}
       className="col-span-6 bg-darkBg md:col-span-3"
     >
-      <Link href="/docs/welcome" legacyBehavior>
+      <Link href="/docs/introduction" legacyBehavior>
         <a className="grid h-full place-content-center text-3xl text-white">
-          <div className="flex space-x-2 items-center">
+          <div className="flex items-center space-x-2">
             <SlDocs />
             <p>Documents</p>
           </div>
@@ -163,24 +174,26 @@ const SocialsBlock = () => (
       </Link>
     </Block>
   </>
-);
+)
 
 const AboutBlock = () => (
   <Block className="col-span-8 text-3xl leading-snug">
     <p>
-      Lets get you started today!<br/>
+      Lets get you started today!
+      <br />
       <span className="text-zinc-400">
-        ZKM is fully open source and available for anyone to build upon leveraging the MIPS instruction set. 
+        ZKM is fully open source and available for anyone to build upon
+        leveraging the MIPS instruction set.
       </span>
     </p>
   </Block>
-);
+)
 
 const AboutBlock2 = () => (
   <Block className="col-span-12">
     <Content />
   </Block>
-);
+)
 
 const Star4 = () => {
   return (
@@ -200,21 +213,21 @@ const Star4 = () => {
 }
 
 const ProjectBlock = () => (
-  <Block className="col-span-2 md:col-span-6 text-3xl leading-snug">
+  <Block className="col-span-2 text-3xl leading-snug md:col-span-6">
     <div className=" flex flex-col gap-3 p-5">
       <Star4 />
       <h4 className="mt-2 text-xl font-heading"></h4>
-       <p>text</p>
+      <p>text</p>
     </div>
   </Block>
-);
+)
 
 const LocationBlock = () => (
   <Block className="col-span-12 flex flex-col items-center gap-4 md:col-span-3">
     <FiMapPin className="text-3xl" />
     <p className="text-center text-lg text-zinc-400">Cyberspace</p>
   </Block>
-);
+)
 
 const EmailListBlock = () => (
   <Block className="col-span-12 md:col-span-9">
@@ -236,7 +249,7 @@ const EmailListBlock = () => (
       </button>
     </form>
   </Block>
-);
+)
 
 const Logo = () => {
   // Temp logo from https://logoipsum.com/
@@ -245,6 +258,6 @@ const Logo = () => {
       src="public/assets/logoW.png"
       alt="avatar"
       className="mb-4 size-14 rounded-full"
-    /> 
-  );
-};
+    />
+  )
+}

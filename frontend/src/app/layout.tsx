dfac4d8c9270df5ad67a2e5d5898cd6cb3574a62
psinelnikov <pavel@sinelnikov.xyz>
2024-09-26 21:32:23 -0600

    Adjusted main page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,10 @@
 import '../../styles/globals.css'
 import React from 'react'
-import type { Metadata } from 'next'
 import { DM_Sans } from 'next/font/google'
 import { ThemeSwitcher } from '@/components/theme-switcher'
 import { ThemeProvider } from '@/components/theme-provider'
 
 const dmSans = DM_Sans({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'ZKM Docs',
-  description: 'The documentation for zkMIPS',
-  icons: {
-    icon: '/icon.ico',
-  },
-  openGraph: {
-    images: [{ url: 'https://docs.zkm.io/assets/zkm-main.jpg' }],
-  },
-}
 
 export default function RootLayout({
   children,
